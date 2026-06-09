aeaab38d7e47dda76f1e5d880ec639a08e798341
Maxmudov Bobur <maxmudovbobur682@gmail.com>
2026-06-09 12:13:38 +0500

    fix: database connection with ssl

--- config/database.js
+++ config/database.js
@@ -2,13 +2,16 @@ const { Sequelize } = require("sequelize");
 
 require("dotenv").config();
 
- const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: "postgres",
+// Neon.tech onlayn bazasiga xavfsiz (SSL) ulanish sozlamasi
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // Neon bazasi rad etmasligi uchun mana shu buyruq shart!
+        }
     }
- );
- module.exports = sequelize
+});
+
+module.exports = sequelize;
